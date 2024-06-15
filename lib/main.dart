2c108ded9454a7f655fd991e04281f1b5dacbe46
elakkiymessi <elakkiyanm.22aim@kongu.edu>
2024-06-15 14:38:25 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:loginuicolors/login.dart';
-import 'package:loginuicolors/register.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyLogin(),
-    routes: {
-      'register': (context) => MyRegister(),
-      'login': (context) => MyLogin(),
-    },
-  ));
+import 'bmipage.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown]);
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+      // routes: <String, WidgetBuilder>{
+      //   '/': (context) => HomePage(),
+      //   '/second': (context) => SecondApp()
+      // },
+    );
+  }
 }
+
+class HomePage extends StatefulWidget {
+  
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // final _textControler = new TextEditingController();
+
+  // @override
+  // void dispose() {
+  //   _textControler.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BmiPage()
+      );
+  }
+}
+
+
